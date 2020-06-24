e9bc083cd24a65643f1597aae7ae1c4fec3016f9
Демидов Сергей Евгеньевич <s.demidov@cft.ru>
2020-06-24 13:40:41 +0700

    feat: add compose and init.sql

---
+++ init.sql
@@ -0,0 +1,31 @@
+\c operationdb
+
+CREATE TABLE languages
+(
+   id serial PRIMARY KEY,
+   name VARCHAR (255) NOT NULL UNIQUE
+);
+
+INSERT INTO languages (name) VALUES ('Паскаль');
+
+CREATE TABLE concepts
+(
+	id serial PRIMARY KEY,
+	name text NOT NULL UNIQUE,
+	category VARCHAR (50) NOT NULL,
+	method VARCHAR (50) NOT NULL,
+	language_id INT REFERENCES languages(id) NOT NULL
+);
+
+INSERT INTO concepts (name, category, method, language_id) VALUES ('Ключевое слово', 'E', 'C', 1);
+
+CREATE TABLE examples
+(
+	id serial PRIMARY KEY,
+	example text NOT NULL,
+	notes text NOT NULL,
+	concept_id INT REFERENCES concepts(id) NOT NULL
+);
+
+
+INSERT INTO examples (example, notes, concept_id) VALUES ('s_star:= s_nov', 'С точностью до пространства возможных схем вычислений', 1);
